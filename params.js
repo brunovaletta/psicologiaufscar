2843f2b2f835209f739f3021ff7e757e42489e37
Bruno Delalibera Valetta <59453854+brunovaletta@users.noreply.github.com>
2020-09-01 20:46:17 -0300

    Update params.js

--- params.js
+++ params.js
@@ -1,2 +1,1 @@
-const params = { stimuli:[cherry,slide,stove,pool,bell,grape,eggplant,box,lizard,scarf,lettuce,cow], index:[1,4,3,3,1,2,2,6,4,6,5,1,4,6,2,5,1,3,10,7,8,11,9,12,11,8,10,9,10,8,12,7,8,9,12,11,5,2,3,4,6,5,3,6,4,4,1,6,5,4,3,6,3,1,8,7,12,11,8,10,9,7,8,9,11,10,8,12,9,7,10,8]}
-
+const params = { stimuli:[cherry,slide,stove,pool,bell,grape,eggplant,box,lizard,scarf,lettuce,cow], index:[3,6,4,1,5,2,5,1,3,4,2,6,4,6,3,5,1,2,11,8,7,12,11,9,8,9,10,12,10,11,11,7,8,10,8,7,5,6,4,2,3,1,3,5,4,4,3,2,2,1,6,5,3,6,11,10,12,9,7,10,9,11,12,7,8,10,8,7,11,7,10,8]}
